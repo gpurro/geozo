290076e1b5db75a1ab76ed970f58c1f36fd07d6a
gPurro <gpurro@seaburysolutions.com>
2020-06-28 19:09:04 -0300

    drupal 8 basic

---
+++ sites/default/settings.local.hostinger.php
@@ -0,0 +1,16 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'u193237823_drupal8',
+  'username' => 'u193237823_drupal8',
+  'password' => 'drupal8',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['trusted_host_patterns'] = array(
+  '^geozo\.site$',
+);
